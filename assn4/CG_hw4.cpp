cb675fabc847986fd30c12430f643d2c5f8676a6
Ari Nguyen <49668914+AriNguyen@users.noreply.github.com>
2020-11-08 06:00:10 -0500

    move specs to CG_hw4.cpp

--- assn4/CG_hw4.cpp
+++ assn4/CG_hw4.cpp
@@ -1,8 +1,7 @@
 /**
- * @file CG_hw3.cpp
- * @brief C++ Program that clip and draw lines from .ps image to .pbm 
+ * @file CG_hw4.cpp
+ * @brief C++ Program that ...
  * @author Ari Nguyen
- *
  */
 #include <cstdio>
 #include <string>
@@ -11,31 +10,36 @@
 
 #include "utils.hpp"
 #include "pbm_file.hpp"
-#include "ps_image.hpp"
 #include "geometry_objects.hpp"
 #include "transformations.hpp"
 #include "drawing.hpp"
+
+struct Specs {
+    char *fileName;
+    float scaleFactor;
+    int rotateAngle, xDim, yDim;
+    // int lowX, lowY, upX, upY;
+    Canva worldView, viewPort, world;
+} specs;
 
 static std::mutex vectorMutex;
 template<typename T>
 void pushToVector(std::vector<T>*, T);
 void transform(Point*, Specs);
+void loadSpecs(int, char *[]);
 
 int main(int argc, char *argv[]) {
-    PSImage psImage;
-    Specs specs;
     PBMFile pbmFile;
     std::vector<Point> pixelPoints;
 
     // handle argvs
-    specs.loadSpecs(argc, argv);
+    loadSpecs(argc, argv);
     pbmFile.setWorld(specs.world);
     pbmFile.setWorldView(specs.worldView);
     pbmFile.setViewPort(specs.viewPort);
     
-    // handle .ps File
-    psImage.setImagePath(specs.fileName);
-    GeoObjects geoObjects = psImage.extractGeoObjects();
+    // handle .smf File
+    
 
     // Hanlde Polygons
     std::vector<Polygon> polygonVector = geoObjects.getPolygons();
@@ -113,3 +117,48 @@ void pushToVector(std::vector<T> *vect, T item) {
     std::lock_guard<std::mutex> lock(vectorMutex);
     vect->push_back(item);
 }   
+
+
+void loadSpecs(int argc, char *argv[]) {
+    // default
+    specs.fileName = (char *)"hw1.ps";
+    specs.scaleFactor = 1.0; 
+    specs.rotateAngle = 0;
+    specs.xDim = 0; 
+    specs.yDim = 0;
+    int x0 = 0, y0 = 0, x1 = 250, y1 = 250; 
+    int x2 = 0, y2 = 0, x3 = 200, y3 = 200; 
+
+    // parse argv
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "-f") == 0) 
+            specs.fileName = argv[++i];
+        else if (strcmp(argv[i], "-s") == 0) 
+            specs.scaleFactor = atof(argv[++i]);
+        else if (strcmp(argv[i], "-r") == 0) 
+            specs.rotateAngle = atoi(argv[++i]);
+        else if (strcmp(argv[i], "-m") == 0) 
+            specs.xDim = std::atoi(argv[++i]);
+        else if (strcmp(argv[i], "-n") == 0) 
+            specs.yDim = std::atoi(argv[++i]);
+        else if (strcmp(argv[i], "-a") == 0) 
+            x0 = std::atoi(argv[++i]);
+        else if (strcmp(argv[i], "-b") == 0) 
+            y0 = std::atoi(argv[++i]);
+        else if (strcmp(argv[i], "-c") == 0) 
+            x1  = std::atoi(argv[++i]);
+        else if (strcmp(argv[i], "-d") == 0) 
+            y1 = std::atoi(argv[++i]);
+        else if (strcmp(argv[i], "-j") == 0) 
+            x2 = std::atoi(argv[++i]);
+        else if (strcmp(argv[i], "-k") == 0) 
+            y2 = std::atoi(argv[++i]);
+        else if (strcmp(argv[i], "-o") == 0) 
+            x3 = std::atoi(argv[++i]);
+        else if (strcmp(argv[i], "-p") == 0) 
+            y3 = std::atoi(argv[++i]);
+    }
+    specs.world.loadDim(0, 0, 500, 500);
+    specs.worldView.loadDim(x0, y0, x1, y1);
+    specs.viewPort.loadDim(x2, y2, x3, y3);
+}

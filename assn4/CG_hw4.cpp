041a862b935c10431e014298e9af10da1bd44346
Ari Nguyen <49668914+AriNguyen@users.noreply.github.com>
2020-11-08 21:48:56 -0500

    change Specs struct

--- assn4/CG_hw4.cpp
+++ assn4/CG_hw4.cpp
@@ -50,10 +50,6 @@ int main(int argc, char *argv[]) {
 void loadSpecs(int argc, char *argv[]) {
     // default
     specs.fileName = (char *)"hw1.ps";
-    specs.scaleFactor = 1.0; 
-    specs.rotateAngle = 0;
-    specs.xDim = 0; 
-    specs.yDim = 0;
     int x0 = 0, y0 = 0, x1 = 250, y1 = 250; 
     int x2 = 0, y2 = 0, x3 = 200, y3 = 200; 
 
@@ -61,30 +57,6 @@ void loadSpecs(int argc, char *argv[]) {
     for (int i = 0; i < argc; i++) {
         if (strcmp(argv[i], "-f") == 0) 
             specs.fileName = argv[++i];
-        else if (strcmp(argv[i], "-s") == 0) 
-            specs.scaleFactor = atof(argv[++i]);
-        else if (strcmp(argv[i], "-r") == 0) 
-            specs.rotateAngle = atoi(argv[++i]);
-        else if (strcmp(argv[i], "-m") == 0) 
-            specs.xDim = std::atoi(argv[++i]);
-        else if (strcmp(argv[i], "-n") == 0) 
-            specs.yDim = std::atoi(argv[++i]);
-        else if (strcmp(argv[i], "-a") == 0) 
-            x0 = std::atoi(argv[++i]);
-        else if (strcmp(argv[i], "-b") == 0) 
-            y0 = std::atoi(argv[++i]);
-        else if (strcmp(argv[i], "-c") == 0) 
-            x1  = std::atoi(argv[++i]);
-        else if (strcmp(argv[i], "-d") == 0) 
-            y1 = std::atoi(argv[++i]);
-        else if (strcmp(argv[i], "-j") == 0) 
-            x2 = std::atoi(argv[++i]);
-        else if (strcmp(argv[i], "-k") == 0) 
-            y2 = std::atoi(argv[++i]);
-        else if (strcmp(argv[i], "-o") == 0) 
-            x3 = std::atoi(argv[++i]);
-        else if (strcmp(argv[i], "-p") == 0) 
-            y3 = std::atoi(argv[++i]);
     }
     specs.world.loadDim(0, 0, 500, 500);
     specs.worldView.loadDim(x0, y0, x1, y1);

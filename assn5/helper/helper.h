3b941a7842603092e27379cbd1ba416a007d7ea8
Ari Nguyen <49668914+AriNguyen@users.noreply.github.com>
2020-12-01 00:38:05 -0500

    copy helper from assn4, change pbm to ppm

---
+++ assn5/helper/helper.h
@@ -0,0 +1,16 @@
+#pragma once 
+
+#include <cstdio>
+#include <climits>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+#include "geometry_objects.h"
+#include "utils.h"
+#include "pbm_file.h"
+#include "smf_file.h"
+#include "transformations.h"
+#include "drawing.h"
---
+++ assn5/helper/transformations.h
@@ -0,0 +1,32 @@
+/**
+ * @file transformations.h
+ * @brief C++ program that holds graphics transformations functions
+ * @author Ari Nguyen
+ *
+ */
+#pragma once
+
+#include <iostream>
+#include <cmath>
+
+#include "geometry_objects.h"
+
+#define PI 3.14259
+
+void scale(int &x, int &y, float scaleFactor) {
+    x = round(x * scaleFactor); 
+    y = round(y * scaleFactor);
+}
+
+void rotate(int &x, int &y, int rotateAngle, geo::vec4D rotatePoint) {
+    int x0 = x;
+    int y0 = y;
+    const float rad = rotateAngle * PI/180;
+    x = round((x0) * cos(rad) - (y0) * sin(rad));
+    y = round((x0) * sin(rad) + (y0) * cos(rad));
+}
+
+void translate(int &x, int &y, int dx, int dy) {
+    x += dx;
+    y += dy;
+}

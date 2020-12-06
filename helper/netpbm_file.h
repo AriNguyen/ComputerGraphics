0619f2d7e0ffe28fc97182c2858818d3d86f24d6
Ari Nguyen <49668914+AriNguyen@users.noreply.github.com>
2020-12-05 22:55:20 -0500

    netpbm: ppm add function that can print out color pixels to ppm file

--- helper/netpbm_file.h
+++ helper/netpbm_file.h
@@ -12,6 +12,7 @@
 #include <cassert>
 
 #include "geometry_objects.h"
+#include "zbuffer.h"
 
 class netpbmFile {
     protected:
@@ -19,84 +20,75 @@ class netpbmFile {
 
     public:
         std::string pixels;
-        geo::canva worldView, viewPort, world;
+        canva<int> worldView, viewPort, world;
 
         netpbmFile() {};
-        virtual void toStdOut(std::vector<geo::point<int>> points) = 0;
+        virtual void toStdOut(std::vector<pixel<int>> points) = 0;
+        virtual void toStdOutB(std::unique_ptr<Color[]> ZBuffer_in) = 0;
 };
 
 class pbmFile : public netpbmFile {
     public:
         pbmFile(char* magic = (char*)"P1") {
             magicNumber = magic;
-        };
+        }
 
-        void toStdOut(std::vector<geo::point<int>> points) {
-        std::cout << magicNumber << "\n";
-        std::cout << world.width << " " << world.height << "\n";
-        std::vector<std::vector<int>> pixelArr(world.width, std::vector<int> (world.height, 0));
-        
-        // debug
-        std::cerr << "\n-----World width & height:  " << world.width << ", " << world.height << "\n";
-        std::cerr << "bottomLeft: " << world.bottomLeft.x << ", " << world.bottomLeft.y << "\n";
-        
-        for (auto &p: points) {
-            // fprintf(stderr, "toStdOut: %d %d\n", p.x, p.y);
-            // std::cerr << p << "\n";
-            assert(p.x <= world.width);
-            assert(p.y <= world.height);
+        void toStdOut(std::vector<pixel<int>> points) {
+            std::cout << magicNumber << "\n";
+            std::cout << world.width << " " << world.height << "\n";
+            std::vector<std::vector<int>> pixelArr(world.width, std::vector<int> (world.height, 0));
             
-            // check if out of index
-            // if (!(p.x <= world.width - 1) || !(p.y <= world.height - 1)) 
-            //     continue;
-            // if (p.x < 0 || p.y < 0)
-            //     continue;
-            pixelArr[p.x - world.bottomLeft.x][p.y - world.bottomLeft.y] = 1;
-        }
-        for (int i = world.height - 1; i >= 0; --i) {
-            std::string row = "";
-            for (int j = 0; j < world.width; ++j) {
-                std::cout << pixelArr[j][i] << " ";
+            // debug
+            std::cerr << "\n-----World width & height:  " << world.width << ", " << world.height << "\n";
+            std::cerr << "bottomLeft: " << world.bottomLeft.x << ", " << world.bottomLeft.y << "\n";
+            
+            for (auto &p: points) {
+                // fprintf(stderr, "toStdOut: %d %d\n", p.x, p.y);
+                // std::cerr << p << "\n";
+                assert(p.x <= world.width);
+                assert(p.y <= world.height);
+                pixelArr[p.x - world.bottomLeft.x][p.y - world.bottomLeft.y] = 1;
             }
-            std::cout << "\n";
+            for (int i = world.height - 1; i >= 0; --i) {
+                std::string row = "";
+                for (int j = 0; j < world.width; ++j) {
+                    std::cout << pixelArr[j][i] << " ";
+                }
+                std::cout << "\n";
+                }
             }
-        }
 };
 
 class ppmFile : public netpbmFile {
+    protected: 
+        int maxColor = 256;
     public:
         ppmFile(char* magic = (char*)"P3") {
             magicNumber = magic;
-        };
-
-        void toStdOut(std::vector<geo::point<int>> points) {
-        std::cout << magicNumber << "\n";
-        std::cout << world.width << " " << world.height << "\n";
-        std::vector<std::vector<int>> pixelArr(world.width, std::vector<int> (world.height, 0));
-        
-        // debug
-        std::cerr << "\n-----World width & height:  " << world.width << ", " << world.height << "\n";
-        std::cerr << "bottomLeft: " << world.bottomLeft.x << ", " << world.bottomLeft.y << "\n";
-        
-        for (auto &p: points) {
-            // fprintf(stderr, "toStdOut: %d %d\n", p.x, p.y);
-            // std::cerr << p << "\n";
-            assert(p.x <= world.width);
-            assert(p.y <= world.height);
-            
-            // check if out of index
-            // if (!(p.x <= world.width - 1) || !(p.y <= world.height - 1)) 
-            //     continue;
-            // if (p.x < 0 || p.y < 0)
-            //     continue;
-            pixelArr[p.x - world.bottomLeft.x][p.y - world.bottomLeft.y] = 1;
         }
-        for (int i = world.height - 1; i >= 0; --i) {
-            std::string row = "";
-            for (int j = 0; j < world.width; ++j) {
-                std::cout << pixelArr[j][i] << " ";
-            }
-            std::cout << "\n";
+
+        // Black white pixel
+        void toStdOut(std::vector<pixel<int>> points) { }
+
+        // Color pixel
+        void toStdOutB(std::unique_ptr<Color[]> ZBuffer_in) {
+            // init z buffer
+            std::vector<std::vector<int>> colorPixelArr(world.width, std::vector<int> (world.height, 0));
+
+            // Print header ppm format
+            std::cout << magicNumber << "\n";
+            std::cout << world.width << " " << world.height << "\n";
+            std::cout << maxColor << "\n";
+
+            // print color pixel
+            int arrLen = world.width * world.height;
+            for (int i = 0; i < arrLen; i++) {
+                // print color 
+                std::cout << ZBuffer_in[i];
+                
+                // new row
+                if ((i > 0) && (i % world.width) == 0) 
+                    std::cout << "\n";
             }
         }
 };
